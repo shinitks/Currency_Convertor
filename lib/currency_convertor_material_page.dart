0f6c90192fe1da881103a5fa079dc738b5c8970e
shinit <shinitks7@gmail.com>
2025-05-08 13:40:21 +0530

    first commit

---
+++ lib/currency_convertor_material_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class CurrencyConvertMaterialPage extends StatelessWidget{
+
+  const CurrencyConvertMaterialPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+  body: Center (
+    child:Text('Helo world'),)
+);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'currency_convertor_material_page.dart';
+
+
+void main(){
+  runApp( const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+
+const MyApp({super.key});
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+home:CurrencyConvertMaterialPage(),
+);
+  }
+}
+
+
